2eb965fd1d068776b5b7289e6fdfb97aafd5f2d5
Anatoly <anatoly.boronilov@megafon.ru>
2025-07-14 14:42:55 +0200

    axum app init

--- secure_storage_backend/src/main.rs
+++ secure_storage_backend/src/main.rs
@@ -1,5 +1,87 @@
+use axum::{Router, routing::get};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use tokio_cron_scheduler::{Job, JobScheduler};
+use tower_http::cors::{Any, CorsLayer};
+use tracing_subscriber::filter::LevelFilter;
+use std::net::TcpListener;
+
+use crate::{config::Config, db::{DBClient, UserExt}};
+
 mod config;
 mod models;
 mod dto;
 mod error;
 mod db;
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .with_test_writer()
+        .init();
+    dotenv().ok();
+
+    let env = Config::new();
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&env.database_url)
+        .await {
+            Ok(pool) => {
+                println!("Connected to database");
+                pool
+            },
+            Err(e) => {
+                eprintln!("Error connecting to database: {}", e);
+                std::process::exit(1);
+            }
+        };
+
+    let db_client = DBClient::new(pool);
+
+    let app_state = AppState { 
+        env: env.clone(), 
+        db_client: db_client.clone() 
+    };
+    let cors = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_headers(Any)
+        .allow_origin(Any)
+        .allow_credentials(true);
+
+    let scheduler = JobScheduler::new().await.unwrap();
+    let job: Job = Job::new_async("0 0 * * * *", move |_, _| {
+        let db_client = app_state.db_client.clone();
+        Box::pin(async move {
+            println!("Running scheduled task to delete expired files");
+            match db_client.delete_expired_files().await {
+                Ok(_) => println!("Expired files deleted successfully"),
+                Err(e) => eprintln!("Error deleting expired files: {}", e),
+            }
+        })
+    }).unwrap();
+
+    scheduler.add(job).await.unwrap();
+
+    tokio::spawn(async move {
+        scheduler.start().await.unwrap();
+    });
+
+    let app = Router::new().layer(cors.clone());
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &env.port)).await.unwrap();
+    println!("Server is running on port {}", &env.port);
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
--- secure_storage_backend/src/models.rs
+++ secure_storage_backend/src/models.rs
@@ -1,19 +1,18 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, sqlx::FromRow, sqlx::Type)]
-#[sqlx(type_name = "user")]
+#[derive(Debug, Clone, Deserialize, Serialize, sqlx::FromRow, sqlx::Type)]
 pub struct User {
     pub id: uuid::Uuid,
     pub name: String,
     pub email: String,
     pub password: String,
     pub public_key: Option<String>,
-    pub created_at: chrono::DateTime<chrono::Utc>,
-    pub updated_at: chrono::DateTime<chrono::Utc>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, sqlx::FromRow, sqlx::Type)]
-#[sqlx(type_name = "file")]
+#[derive(Debug, Clone, Deserialize, Serialize, sqlx::FromRow, sqlx::Type)]
 pub struct File {
     pub id: uuid::Uuid,
     pub user_id: Option<uuid::Uuid>,
@@ -22,36 +21,33 @@ pub struct File {
     pub encrypted_aes_key: Vec<u8>,
     pub encrypted_file: Vec<u8>,
     pub iv: Vec<u8>,
-    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub created_at: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, sqlx::FromRow, sqlx::Type)]
-#[sqlx(type_name = "shared_link")]
+#[derive(Debug, Clone, Deserialize, Serialize, sqlx::FromRow, sqlx::Type)]
 pub struct SharedLink {
     pub id: uuid::Uuid,
     pub file_id: Option<uuid::Uuid>,
-    pub recipient_user_id: uuid::Uuid,
+    pub recipient_user_id: Option<uuid::Uuid>,
     pub password: String,
-    pub expiration_date: chrono::DateTime<chrono::Utc>,
-    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub expiration_date: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>,
 }
 
 #[derive(sqlx::FromRow)]
 pub struct SentFileDetails {
     pub file_id: uuid::Uuid,
     pub file_name: String,
-    pub file_size: i64,
     pub recipient_email: String,
-    pub created_at: chrono::DateTime<chrono::Utc>,
-    pub expiration_date: chrono::DateTime<chrono::Utc>,
+    pub expiration_date: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>
 }
 
 #[derive(sqlx::FromRow)]
 pub struct ReceiveFileDetails {
     pub file_id: uuid::Uuid,
     pub file_name: String,
-    pub expiration_date: chrono::DateTime<chrono::Utc>,
     pub sender_email: String,
-    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub expiration_date: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>
 }
-
